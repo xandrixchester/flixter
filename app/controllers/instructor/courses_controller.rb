0d8eb0f9f9e8ee261c88fc3abbcd1491ee1f9e69
xandrix <xandrixecklor@hotmaill.com>
2019-04-28 05:27:55 +0000

    Course Validation

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -5,10 +5,14 @@ class Instructor::CoursesController < ApplicationController
 		@course = Course.new
 	end
 
-	 def create
-    @course = current_user.courses.create(course_params)
-    redirect_to instructor_course_path(@course)
-  end
+ def create
+	  @course = current_user.courses.create(course_params)
+	  if @course.valid?
+	    redirect_to instructor_course_path(@course)
+	  else
+	    render :new, status: :unprocessable_entity
+	  end
+	end
 
   def show
     @course = Course.find(params[:id])
