1ad52c5fa5c22740a60a56906faf336e1acc0b6b
xandrix <xandrixecklor@hotmaill.com>
2019-05-13 19:35:21 +0000

    student enrollment 2

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
 root 'static_pages#index'
 
   resources :courses, only: [:index, :show] do
-    resources :enrollments, only :create
+    resources :enrollments, only: :create
   end
+  
   resources :lessons, only: [:show]
   namespace :instructor do
     resources :sections, only: [] do
